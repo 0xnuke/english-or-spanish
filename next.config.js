5e4b4b19844b7620abe569880ae37730a3e35143
suppawich <nukza_11102@hotmail.com>
2024-07-12 20:17:01 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config, { isServer }) => {
+    // Add file-loader rule for mp3 files
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
