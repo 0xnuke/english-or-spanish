0610948360c6588136dbfb6c45b0ca7534a66d32
tinnn <tinnnapon.chotepachroj@gmail.com>
2024-07-13 17:32:25 +0700

    back code

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,9 @@ import {
   IAgoraRTCClient,
   IRemoteAudioTrack,
 } from 'agora-rtc-sdk-ng';
-import ricardo from '../public/ricardo.png';
 import Image from 'next/image';
-import img from "../public/bg-1.png"
-import html2canvas from "html2canvas";
-
+import img from '../public/bg-1.png';
+import html2canvas from 'html2canvas';
 
 type TCreateRoomResponse = {
   room: Room;
@@ -111,6 +109,7 @@ async function connectToAgoraRtm(
       message: message.text,
     });
   });
+  // playSound();
 
   return {
     channel,
@@ -132,8 +131,13 @@ export default function Home() {
   const [imageSrc, setImageSrc] = useState('');
 
   const isMoved = useRef(false);
+  const isSnap = useRef(false);
   const channelRef = useRef<RtmChannel>();
   const rtcClientRef = useRef<IAgoraRTCClient>();
+
+  useEffect(() => {
+    playSound();
+  }, [themVideo]);
 
   async function connectToAgoraRtc(
     roomId: string,
@@ -156,6 +160,7 @@ export default function Home() {
       token,
       userId
     );
+    let themUsers;
 
     client.on('user-published', (themUser, mediaType) => {
       client.subscribe(themUser, mediaType).then(() => {
@@ -163,8 +168,13 @@ export default function Home() {
           const remoteVideoTrack =
             themUser.videoTrack as IExtendedRemoteVideoTrack;
           onVideoConnect(remoteVideoTrack);
-          detectMotion(remoteVideoTrack, false);
-          setWhoMoved('another');
+          if (themUser) {
+            themUsers = themUser;
+            setTimeout(() => {
+              detectMotion(remoteVideoTrack, false, 'another');
+              setWhoMoved('another');
+            }, 2000); // Start detectMotion after 2 seconds
+          }
         }
         if (mediaType === 'audio') {
           onAudioConnect(themUser.audioTrack);
@@ -178,8 +188,13 @@ export default function Home() {
       (track) => track.trackMediaType === 'video'
     ) as IExtendedCameraVideoTrack;
     onWebcamStart(cameraTrack);
-    detectMotion(cameraTrack, true);
-    setWhoMoved('you');
+    if (themUsers) {
+      setTimeout(() => {
+        detectMotion(cameraTrack, true, 'you');
+        setWhoMoved('you');
+      }, 2000); // Start detectMotion after 2 seconds
+    }
+
     await client.publish(tracks);
 
     return { tracks, client };
@@ -208,9 +223,33 @@ export default function Home() {
     setInput('');
   }
 
+  function getImage(user: string) {
+    console.log('getting image', whoMoved);
+    let videoConrainerId;
+    if (user === 'you') {
+      console.log('you logger', whoMoved);
+      videoConrainerId = document.getElementById('video-container-you');
+    } else if (user === 'another') {
+      videoConrainerId = document.getElementById('video-container-another');
+    }
+
+    if (videoConrainerId && isSnap.current === false) {
+      html2canvas(videoConrainerId, {
+        width: 400,
+        height: 320,
+      }).then((canvas) => {
+        const imageURL = canvas.toDataURL('image/jpeg', 1.0);
+        console.log('img logger');
+        setImageSrc(imageURL);
+      });
+    }
+    isSnap.current = true;
+  }
+
   function detectMotion(
     videoTrack: IExtendedRemoteVideoTrack | IExtendedCameraVideoTrack,
-    isLocal: boolean
+    isLocal: boolean,
+    user: string
   ) {
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
@@ -244,7 +283,8 @@ export default function Home() {
           // Motion threshold
           isMoved.current = true;
           setIsDetected(true);
-          playSound();
+          isSnap.current === false && setInterval(getImage, 50, user);
+          // playSound();
         }
       }
       lastImageData = imageData;
@@ -253,22 +293,6 @@ export default function Home() {
     // Check for motion every second
     setInterval(checkForMotion, 2000);
     setInterval(getImage, 5000);
-  }
-
-  function getImage() {
-    const videoConrainerId = document.getElementById(
-      "video-container-1"
-    );
-    if(videoConrainerId){
-    html2canvas(videoConrainerId, {
-      width: 400,
-      height: 320,
-    }).then((canvas) => {        
-      const imageURL = canvas.toDataURL('image/jpeg', 1.0);
-      console.log("img logger");
-      setImageSrc(imageURL);
-    });
-    }
   }
 
   async function connectToARoom() {
@@ -299,6 +323,7 @@ export default function Home() {
         (message: TMessage) => setMessages((cur) => [...cur, message]),
         rtmToken
       );
+
       channelRef.current = channel;
 
       const { tracks, client } = await connectToAgoraRtc(
@@ -364,27 +389,63 @@ export default function Home() {
         {isChatting ? (
           <>
             {room._id}
+            {imageSrc && (
+              <img src={imageSrc} style={{ width: '100%', height: 'auto' }} />
+            )}
             <button onClick={handleNextClick}>next</button>
             <div className='chat-window'>
               <div className='video-panel'>
-              <div className="video-stream" id="video-container-1">
-                    
-                    {myVideo && (
-                      <VideoPlayer
-                      style={{ width: "100%", height: "100%", position: "absolute" }}
+                <div className='video-stream' id='video-container-you'>
+                  {myVideo && (
+                    <VideoPlayer
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                        position: 'absolute',
+                      }}
                       videoTrack={myVideo}
-                      />
-                      )}
-                    {isMoved.current && whoMoved === "you" && <Image src={img} alt="Ricardo"  style={{position: "absolute", zIndex: 100, width: "100%", height: "auto", top: 0,left: 0}}/>}
-                  </div>
-                <div className="video-stream">
+                    />
+                  )}
+                  {isMoved.current && whoMoved === 'you' && (
+                    <Image
+                      src={img}
+                      alt='Ricardo'
+                      style={{
+                        position: 'absolute',
+                        zIndex: 100,
+                        width: '100%',
+                        height: 'auto',
+                        top: 0,
+                        left: 0,
+                      }}
+                    />
+                  )}
+                </div>
+                <div className='video-stream' id='video-container-another'>
                   {themVideo && (
                     <VideoPlayer
-                      style={{ width: "100%", height: "100%", position: "absolute" }}
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                        position: 'absolute',
+                      }}
                       videoTrack={themVideo}
                     />
-                    )}
-                    {isMoved.current && whoMoved === "another" && <Image src={img} alt="Ricardo"  style={{position: "absolute", zIndex: 100, width: "100%", height: "auto", top: 0,left: 0}}/>}
+                  )}
+                  {isMoved.current && whoMoved === 'another' && (
+                    <Image
+                      src={img}
+                      alt='Ricardo'
+                      style={{
+                        position: 'absolute',
+                        zIndex: 100,
+                        width: '100%',
+                        height: 'auto',
+                        top: 0,
+                        left: 0,
+                      }}
+                    />
+                  )}
                 </div>
               </div>
 
@@ -402,16 +463,22 @@ export default function Home() {
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
                   ></input>
-                  {imageSrc && <img src={imageSrc} style={{ width: "100%", height: "auto"}}/>}
+                  {imageSrc && (
+                    <img
+                      src={imageSrc}
+                      style={{ width: '100%', height: 'auto' }}
+                    />
+                  )}
                   <button>submit</button>
                 </form>
               </div>
-
               <div>
                 <button onClick={clearIsDetected}>clear</button>
-                <button onClick={() => console.log(isMoved)}>logger</button>
+                <button onClick={() => console.log(isSnap)}>logger</button>
                 <button onClick={playSound}>Boop!</button>
-                <button onClick={()=> console.log("logger",imageSrc)}>img logger</button>
+                <button onClick={() => console.log('logger', imageSrc)}>
+                  img logger
+                </button>
                 <audio
                   ref={audioRef}
                   controls
